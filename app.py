4d275a588c8641fc38f412642ff4a63c7c4663c4
olamide <senayonolamide10@gmail.com>
2026-03-03 10:47:50 +0000

    learnt how print works

---
+++ app.py
@@ -0,0 +1,10 @@
+# this is another comment
+# This is yet another  
+# this is yet another
+
+print(trouser_quantity,"trouser")
+print("each trouser costs",trous trouser quantity =3 # number of trouser bought
+# trouser _cost =750 # cost of one trouser
+# total_cost # trouser_cost *trouser_cost # totalamount paid for trousers
+print("each trouser cost";trouser_cost)
+
