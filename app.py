e6304c278aa554ae8a0ce1998402e7220110898e
olamide <senayonolamide10@gmail.com>
2026-03-04 10:18:08 +0000

    Learnt about escape sequence \n \t

--- app.py
+++ app.py
@@ -2,9 +2,4 @@
 # This is yet another  
 # this is yet another
 
-print(trouser_quantity,"trouser")
-print("each trouser costs",trous trouser quantity =3 # number of trouser bought
-# trouser _cost =750 # cost of one trouser
-# total_cost # trouser_cost *trouser_cost # totalamount paid for trousers
-print("each trouser cost";trouser_cost)
 
